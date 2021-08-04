99a9009b87db46e8d3c817b4e9aef0081a98dab9
aayushali <aayushkhan51@gmail.com>
2021-08-04 20:59:07 +0545

    working on dispatch

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
 import './index.css';
 import App from './components/App';
+import movies from './reducers/index';
+
+const store = createStore(movies);
+
+console.log(store);
+console.log('Before State' , store.getState());
+
+store.dispatch({
+    type: 'ADD_MOVIES',
+    movies: [{name: 'Superman'}]
+});
+
+console.log('After State' , store.getState());
 
 ReactDOM.render(
   <React.StrictMode>
